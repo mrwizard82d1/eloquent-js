de36492edcaabaec4f8cbd2967af116312df4bfb
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-08-28 14:38:46 -0500

    Write a "chess board" pattern

---
+++ ch02/chess_board_1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by larry on 8/28/16.
+ */
+
+for (var rank = 0; rank < 8; rank++) {
+    if (rank % 2 == 0) {
+        console.log('# # # # ');
+    }
+    else {
+        console.log(' # # # #');
+    }
+}
